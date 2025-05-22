4ada322b1dd8268bd5d4a34f50365d92c812ce9c
Nicolas Engles <77967335+nicolasengles@users.noreply.github.com>
2025-05-21 21:45:04 -0300

    adicionada classe tela e metodo main

--- emhm.py
+++ emhm.py
@@ -1,14 +1,25 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+import tela
 
-janela = Tk()
-janela.geometry('1280x720')
-janela.title('Expedição do Milhão: Histórias e Mapas')
-arquivo_imagem_fundo = PhotoImage(file='images/imagemfundo0.png')
-imagem_fundo = Label(janela, image = arquivo_imagem_fundo)
-imagem_fundo.place(x = -2, y = 0)
-tela = ttk.Frame(janela, width=1280, height=720)
-tela.grid()
-ttk.Label(tela, text="Hello World!").place(x=0,y=0)
-ttk.Button(tela, text="Quit", command=janela.destroy).grid(column=1, row=0)
-janela.mainloop()
+tela_atual = None
+
+def main():
+    #  Criar Janela
+    janela = tk.Tk()
+    janela.title('Expedição do Milhão: Histórias e Mapas')
+    janela.minsize(1280, 720)
+    janela.maxsize(1280, 720)
+
+    #  Definir imagem de fundo
+    arquivo_imagem_fundo = tk.PhotoImage(file='images/imagemfundo0.png')
+    imagem_fundo = tk.Label(janela, image = arquivo_imagem_fundo)
+    imagem_fundo.place(x = -2, y = -2)
+
+    #  Definir tela_atual aqui:
+    tela_atual = tela.MenuPrincipal(janela)
+
+    #  NÃO MEXER NISSO
+    janela.mainloop()
+
+if __name__ == "__main__":
+    main()
---
+++ tela.py
@@ -0,0 +1,11 @@
+# Template para a criação de uma nova tela:
+
+# class NomeDaTela(ttk.Frame):
+#     def __init__(self, master):
+#         super().__init__(master)
+
+from tkinter import ttk
+
+class MenuPrincipal(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
