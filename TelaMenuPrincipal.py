c4aa132760b7fc1d8356d2a1d33a0ac0a420c921
Nicolas Mota Engles <77967335+nicolasengles@users.noreply.github.com>
2025-05-29 08:37:45 -0300

    TelaMenuPrincipal refatorada; Adicionados modulos jogo e partida

--- TelaMenuPrincipal.py
+++ TelaMenuPrincipal.py
@@ -6,54 +6,53 @@ from TelaPartida import *
 class TelaMenuPrincipal(tk.Canvas):
     def __init__(self, master, width=1280, height=720, highlightthickness=0):
         super().__init__(master)
-        
-        # --- Load & resize images ---
-        bg_img         = tk.PhotoImage(file='images/imagemfundo0.png')
-        raw_title      = Image.open(r"images/EXPEDICAO_DO_MILHAO.png").resize((656, 62), Image.LANCZOS)
-        title_img      = ImageTk.PhotoImage(raw_title)
-        raw_button_img = Image.open(r"images/Frame.png").resize((203, 58), Image.LANCZOS)
-        button_img     = ImageTk.PhotoImage(raw_button_img)
-        raw_button_2_img = Image.open(r"images/Frame_3.png").resize((203,58), Image.LANCZOS)
-        button_2_img   = ImageTk.PhotoImage(raw_button_2_img)
+        # Carregar imagens
+        imagem_fundo         = tk.PhotoImage(file='images/imagemfundo0.png')
+        arquivo_imagem_titulo      = Image.open(r"images/EXPEDICAO_DO_MILHAO.png").resize((656, 62), Image.LANCZOS)
+        imagem_titulo      = ImageTk.PhotoImage(arquivo_imagem_titulo)
+        arquivo_imagem_botao1 = Image.open(r"images/Frame.png").resize((203, 58), Image.LANCZOS)
+        imagem_botao1     = ImageTk.PhotoImage(arquivo_imagem_botao1)
+        arquivo_imagem_botao2 = Image.open(r"images/Frame_3.png").resize((203,58), Image.LANCZOS)
+        imagem_botao2   = ImageTk.PhotoImage(arquivo_imagem_botao2)
 
-        # --- Create canvas and draw background & title ---
+        # Criar canvas e adicionar título e imagem de fundo
         self.pack(fill="both", expand=True)
-        self.create_image(0, 0, image=bg_img,     anchor=tk.NW)
-        self.create_image(640, 200, image=title_img, anchor='n')
+        self.create_image(0, 0, image=imagem_fundo, anchor=tk.NW)
+        self.create_image(640, 200, image=imagem_titulo, anchor='n')
 
-        # --- First button area ---
-        btn_x, btn_y = 640, 200 + 62 + 15
-        btn_w, btn_h = 203, 58
+        # Primeiro botão
+        botao_x, botao_y = 640, 200 + 62 + 15
+        comprimento_botao, altura_botao = 203, 58
 
-        rect1 = self.create_rectangle(
-            btn_x - btn_w/2, btn_y,
-            btn_x + btn_w/2, btn_y + btn_h,
+        retangulo1 = self.create_rectangle(
+            botao_x - comprimento_botao/2, botao_y,
+            botao_x + comprimento_botao/2, botao_y + altura_botao,
             fill='', outline=''
         )
-        img1 = self.create_image(btn_x, btn_y, image=button_img, anchor='n')
+        imagem1 = self.create_image(botao_x, botao_y, image=imagem_botao1, anchor='n')
 
         def on_first_click(event):
             janela.Janela.mudar_tela(tela_nova=TelaPartida)
 
-        for tag in (rect1, img1):
+        for tag in (retangulo1, imagem1):
             self.tag_bind(tag, '<Button-1>', on_first_click)
 
-        # --- Second button right below the first ---
-        btn2_y = btn_y + btn_h + 30  # 30px gap
-        rect2 = self.create_rectangle(
-            btn_x - btn_w/2, btn2_y,
-            btn_x + btn_w/2, btn2_y + btn_h,
+        # Segundo botão
+        botao2_y = botao_y + altura_botao + 30
+        retangulo2 = self.create_rectangle(
+            botao_x - comprimento_botao/2, botao2_y,
+            botao_x + comprimento_botao/2, botao2_y + altura_botao,
             fill='', outline=''
         )
-        img2 = self.create_image(btn_x, btn2_y, image=button_2_img, anchor='n')
+        imagem2 = self.create_image(botao_x, botao2_y, image=imagem_botao2, anchor='n')
 
         def on_second_click(event):
             print("Segundo botão clicado")
 
-        for tag in (rect2, img2):
+        for tag in (retangulo2, imagem2):
             self.tag_bind(tag, '<Button-1>', on_second_click)
 
-        # --- Keep references to prevent GC ---
-        master.bg_img     = bg_img
-        master.title_img  = title_img
-        master.button_img = button_img
+        # Manter imagens na memória
+        master.bg_img = imagem_fundo
+        master.title_img = imagem_titulo
+        master.button_img = imagem_botao1
